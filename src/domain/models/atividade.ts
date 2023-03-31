d0eccf68bfa6a1dc5ffac74d79f1facb58f62772
Weriks Santos <werikscs@gmail.com>
2023-03-31 19:55:20 -0300

    feat: create class Atividade

---
+++ src/domain/models/atividade.ts
@@ -0,0 +1,14 @@
+import PacoteTrabalho from './pacote-trabalho'
+import { Projeto } from './projeto'
+
+export default class Atividade {
+  constructor(
+    readonly id: string,
+    readonly projeto: Projeto,
+    readonly pacoteTrabalho: PacoteTrabalho,
+    readonly title: string,
+    readonly tempoHorasUteis: number,
+    readonly inicio: Date,
+    readonly orientacoes: string,
+  ) {}
+}
