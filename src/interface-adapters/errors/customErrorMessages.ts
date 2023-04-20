bb2b4ffa062bf850debee7ae0ffed8c623401ec6
Weriks Santos <werikscs@gmail.com>
2023-04-20 00:13:27 -0300

    feat: create enum with message erros

---
+++ src/interface-adapters/errors/customErrorMessages.ts
@@ -0,0 +1,7 @@
+const enum CustomErrorMessages {
+  USER_NOT_REGISTERED = 'User not registered',
+  INCORRECT_CREDENTIALS = 'Incorrect Credentials',
+  EMAIL_ALREADY_TAKEN = 'Email already taken',
+}
+
+export default CustomErrorMessages
